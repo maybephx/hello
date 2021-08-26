fb26bf90f7dfa82037263b2f66270ec0eb4b0b25
Phoenix Fletcher <cfletcher6@huskers.unl.edu>
2021-08-26 19:40:58 +0000

    initial commit

---
+++ hello/hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Phoenix Fletcher
+ *         cfletcher6@huskers.unl.edu
+ *         Peter McAndrews
+ *         pmcandrews3@huskers.unl.edu
+ * Date: 2021/24/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Phoenix, Peter\n");
+
+  return 0;
+}
